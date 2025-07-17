82e581463f09121dfdd80f962885998a37905c4a
Informadigga <122704980+informadigga@users.noreply.github.com>
2025-07-17 22:56:30 +0200

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,5 +1,5 @@
-import pkg from 'pg';
-const { Pool } = pkg;
+import * as pg from 'pg';
+const { Pool } = pg.default || pg;
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from "../shared/schema";
 
